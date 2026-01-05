ffb30ff642909afb54e3f5d2854d1986ff48e21d
Nathan Dolor <ndolor@westtg.com>
2026-01-04 20:00:58 -0400

    fix: update header title to 'MVAA Co-op Accounting'

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Accounts from "./components/Accounts"
 export default function App() {
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>Accounting for sumthing idk</h1>
+      <h1>MVAA Co-op Accounting</h1>
       <Accounts />
     </div>
   )
